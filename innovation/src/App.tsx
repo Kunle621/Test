99f8496035c1d6bef5840ca892ac39a84f924a6e
Yahya <kunlebiola12@gmail.com>
2025-07-19 13:51:21 +0200

    learn react

--- innovation/src/App.tsx
+++ innovation/src/App.tsx
@@ -1,16 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Navbar, Container, Offcanvas, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+
+  const handleClose = () => setShowOffcanvas(false);
+  const handleShow = () => setShowOffcanvas(true);
 
   return (
     <>
-      
+      <Navbar bg="light" fixed="top" expand={false}>
+        <Container fluid>
+          <Navbar.Brand href="#">STAMINA</Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" onClick={handleShow} />
+          <Navbar.Offcanvas
+            show={showOffcanvas}
+            onHide={handleClose}
+            placement="end"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#" active>Home</Nav.Link>
+                <Nav.Link href="#">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
+                  <NavDropdown.Item href="#">Action 1</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Action 2</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form className="d-flex mt-3" onSubmit={e => e.preventDefault()}>
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success" type="submit">Search</Button>
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+      <body>
+        
+      </body>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
